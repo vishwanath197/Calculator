02a58b38373ccf1286ede3d4b709927449de8340
vishwanath197 <73850817+vishwanath197@users.noreply.github.com>
2020-11-30 18:21:33 +0530

    Add files via upload

---
+++ MinutesToSeconds.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class mintotime {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+
+		
+		System.out.print("Enter the number of minutes: ");
+		int minutes = input.nextInt();
+
+		
+		int years = minutes / 525600;
+		int days = (minutes % 525600) / 1440;
+
+		
+		System.out.println(minutes + " minutes is approximately " + years
+			+ " years and " + days + " days");
+	}
+}
